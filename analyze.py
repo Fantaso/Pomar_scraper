65390b8237fe98d989d10dcad27ea0dae7219128
Carlos Rosas <fantaso.code@gmail.com>
2018-11-23 19:35:56 +0100

    started with pandas library to play around with the data

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# name of the table headers
+col_names = ['cedula','genre','city','name','last_name','phone','birthday','email','email_alt','city_attend','experience','cata','degustacion','course','visit','event','ninguna','date_found']
+# read file, has no headers names, add the col names and the table is comma ',' separated
+table = pd.read_table('database_pomar3.txt', header=None, names=headers, sep=',')
+
+# print table
+print(table.head())
+print(table)
